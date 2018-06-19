76bf7d77065bd737eafcd5478f3751c167297ddb
unknown <jiangwei@mvwchina.com>
2018-06-19 16:12:26 +0800

    second commint

--- src/test/java/testcase/JxhdYw.java
+++ src/test/java/testcase/JxhdYw.java
@@ -25,7 +25,7 @@ public class JxhdYw {
 		//医院管理员testyiyuan
 		//driver.get("https://newlztest.mvwchina.com/pc/manage/index.html?token=bce9f4a746d244ebb8f10fd869ef1e28");
 		//住培2.0新建管理员用户huanggl
-		driver.get("https://newlztest.mvwchina.com/pc/manage/index.html?token=95546727337c4f66b06f6a034d1c43a0");
+		driver.get("https://ccgme-cttms.oss-cn-beijing.aliyuncs.com/pc/manage/index.html?token=baa959e6bf434e66a482edd5df601a57");
 	}
 	@Test(dependsOnMethods="login")
 	public void AddHuo() throws Exception{
